db6f183094d3cf5fa53a137f51d0ab6dc754976a
trofimovaaa <atrofimova2003@mail.ru>
2023-05-14 14:42:48 +0300

    jwt auth2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // разрешаем кросс-доменные запросы
 
   const config = new DocumentBuilder()
     .setTitle('Education API')
